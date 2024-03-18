950db79cba2de32fa20c3c75e0c98e797e66c3ee
ae <erradi@live.com>
2024-03-18 08:28:26 +0300

    Updated kNN and Naive Bayes slides and examples. Plus draft regression slides and examples

--- examples/07.kNN-NaiveBayes/knn_classifer.py
+++ examples/07.kNN-NaiveBayes/knn_classifer.py
@@ -1,21 +1,21 @@
+# This file contains the implementation of the KNN classifier algorithm
+# This implementation is based on this video tutorial https://www.youtube.com/watch?v=rTEtEy5o3X0
 import numpy as np
 from collections import Counter
 
 def euclidean_distance(x1, x2):
-    distance = np.sqrt(np.sum((x1-x2)**2))
-    return distance
+    return np.sqrt(np.sum((x1-x2)**2))
 
 class KNN:
     def __init__(self, k=3):
         self.k = k
 
-    def fit(self, X, y):
-        self.X_train = X
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
         self.y_train = y
 
     def predict(self, X):
-        predictions = [self._predict(x) for x in X]
-        return predictions
+        return [self._predict(x) for x in X]
 
     def _predict(self, x):
         # compute the distance
@@ -25,6 +25,6 @@ class KNN:
         k_indices = np.argsort(distances)[:self.k]
         k_nearest_labels = [self.y_train[i] for i in k_indices]
 
-        # majority voye
+        # majority vote
         most_common = Counter(k_nearest_labels).most_common()
         return most_common[0][0]
